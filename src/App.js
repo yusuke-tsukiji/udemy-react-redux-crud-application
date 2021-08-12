e2bf453eb38e6bb878288027e058e85c5552575c
yusuke-tsukiji <yusuke0314z@gmail.com>
2021-08-12 18:02:24 +0900

    create class component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,23 @@ import React from "react";
 function App() {
   const greeting = "Hello!!";
   const dom = <h1 className="foo">{greeting}</h1>;
+  // return (
+  //   <React.Fragment>
+  //     <label htmlFor="bar">bar</label>
+  //     <input id="bar" type="text" onChange={() => {console.log("Hi!!")}}></input>
+  //   </React.Fragment>
+  // )
+
+  const Cat = () => {
+    return <div>Meow!!</div>
+  }
+
   return (
     <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input id="bar" type="text" onChange={() => {console.log("Hi!!")}}></input>
+      <Cat/>
+      <Cat/>
+      <Cat/>
+      <Cat/>
     </React.Fragment>
   )
 }
