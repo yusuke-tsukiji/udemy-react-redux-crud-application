b42a89ab7328aa657f102e24fa846d71296a68a5
yusuke-tsukiji <yusuke0314z@gmail.com>
2021-08-12 17:36:26 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import React from "react";
+
 function App() {
-  return <div><h1>Hello, world!</h1></div>;
+  const greeting = "Hello!!";
+  const dom = <h1 className="foo">{greeting}</h1>;
+  return (
+    <React.Fragment>
+      <label htmlFor="bar">bar</label>
+      <input id="bar" type="text" onChange={() => {console.log("Hi!!")}}></input>
+    </React.Fragment>
+  )
 }
 
 export default App;
