23ff09155662483bed8c6a1697fec494fd0da496
yusuke-tsukiji <yusuke0314z@gmail.com>
2021-08-13 12:31:45 +0900

    implement store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
 import './index.css';
-import App from './App';
+import reducer from './reducers'
+
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
